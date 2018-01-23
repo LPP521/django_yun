cd3da6625a4e52a07cb30a9116f44baf87d411bd
rm3f <cg7tub@163.com>
2018-01-23 22:38:02 +0800

    创建数据库字段

--- mpan/models.py
+++ mpan/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    _file_id = models.AutoField(
+        primary_key=True
+    )
+    _file_name = models.CharField(
+        max_length=225,
+        verbose_name="文件名称",
+        default=''
+    )
+    file_type = (
+        ('apk', 'apk'),
+        ('exe', 'exe'),
+        ('unknow', 'unknow'),
+    )
+    _file_type = models.CharField(
+        max_length=50,
+        choices=file_type,
+        default='',
+    )
+    _file_size = models.IntegerField(
+        verbose_name="文件大小",
+        default=0
+    )
+    _file_path = models.CharField(
+        max_length=225,
+        verbose_name="文件路径",
+        default=''
+    )
+    file_md5 = models.CharField(
+        max_length=50,
+        default=''
+    )
+    _upload_data = models.DateField(
+        auto_now=True
+    )
+    _upload_time = models.TimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return str(self._file_name)
--- my_pan/settings.py
+++ my_pan/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_pan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_pan',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PASSWORD': 'qwqw',
+        'PORT': 3306,
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
