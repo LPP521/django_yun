81d97180701ceb75c2f743bb280f9cc41fd4bf47
rm3f <cg7tub@163.com>
2018-01-24 04:23:28 +0800

    编写api及ajax请求

--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from mpan.models import File
+import json
 
 
 def api_lixian(requests):
-    return HttpResponse("asdasdas")
+    dic = {}
+    for a in list(File.objects.all()):
+        dic[str(a._file_id)] = a._file_name
+    return HttpResponse(json.dumps(dic), content_type='application/json; charset=utf-8')
