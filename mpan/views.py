d2e4513913da4932f71afe78d4e633f8ab1f241b
rm3f <cg7tub@163.com>
2018-01-24 01:21:49 +0800

    添加模板

--- mpan/views.py
+++ mpan/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
-def file_list():
-    return render()
+def file_list(request):
+    return render(request, "file_list.html")
+
+
+def github(request):
+    return render(request, "github.html")
+
+
+def about(request):
+    return render(request, "about.html")
--- my_pan/urls.py
+++ my_pan/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from my_pan.views import *
+from mpan.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('file_list/', file_list),
+    path('github/', github),
+    path('about/', about)
 ]
